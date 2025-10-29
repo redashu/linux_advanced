02b475661ac46d4f74b21ec65b051f5489f2e4cc
Ashutoshh Singh <asashuthegreat4@gmail.com>
2025-10-29 07:50:36 +0530

    Create code.c

---
+++ troubleshooting/code.c
@@ -0,0 +1,6 @@
+#include <stdio.h>   // Include standard input-output library
+
+int main() {
+    printf("Hello, World!\n");  // Print message to the screen
+    return 0;                   // Exit the program successfully
+}
